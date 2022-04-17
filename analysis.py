7146ae55c698635206f4f6019aa4fdc2d87438d7
ruthelgiana <102641992+ruthelgiana@users.noreply.github.com>
2022-04-17 16:44:30 +0700

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -2,3 +2,12 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv("01224099999.csv")
+
+dt$station = station
+dt$station = as.numeric(format(dt$station, “%Y”))
+dt$station = as.numeric(format(dt$station, “%y”))
+
+dt$station = date
+dt$date = as.numeric(format(dt$date, “%m”))
+dt$date = format(dt$date, “%b”)
+dt$date = format(dt$date, “%B”)
