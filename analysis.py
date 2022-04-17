3e0bc0a19f1ae44a71bb5be309b5278fc9327a1b
ruthelgiana <102641992+ruthelgiana@users.noreply.github.com>
2022-04-17 16:49:03 +0700

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -7,7 +7,7 @@ dt$station = station
 dt$station = as.numeric(format(dt$station, “%Y”))
 dt$station = as.numeric(format(dt$station, “%y”))
 
-dt$station = station
+dt$station = date
 dt$date = as.numeric(format(dt$date, “%m”))
 dt$date = format(dt$date, “%b”)
 dt$date = format(dt$date, “%B”)
